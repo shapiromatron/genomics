cbffc82b77cf751ee3e88ccfe971aa1a0b305621
Andy Shapiro <shapiromatron@gmail.com>
2015-10-20 17:25:40 -0400

    get subset of matrix w/ interpolation via API

--- project/genomics/api.py
+++ project/genomics/api.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 from utils.api import SiteMixin
 
@@ -16,3 +20,23 @@ class ResultViewset(SiteMixin, viewsets.ModelViewSet):
         return models.Result.objects\
             .filter(user=self.request.user)\
             .order_by('last_updated')
+
+    @detail_route(methods=['get'])
+    def dataset(self, request, pk):
+
+        start = 0
+        try:
+            start = int(self.request.query_params.get('start', start))
+            start = max(start, 0)
+        except ValueError:
+            pass
+
+        width = -1
+        try:
+            width = int(self.request.query_params.get('width', width))
+        except ValueError:
+            pass
+
+        object_ = get_object_or_404(models.Result, pk=pk)
+        response = object_.get_heatmap_dataset(start, width)
+        return Response(response)
--- project/genomics/models.py
+++ project/genomics/models.py
@@ -1,5 +1,8 @@
 import cPickle
 import logging
+import numpy as np
+from scipy.misc import imresize
+
 from django.db import models
 from django.conf import settings
 from django.core.cache import cache
@@ -35,4 +38,57 @@ class Result(models.Model):
     def get_absolute_url(self):
         return reverse('genomics:result_detail', args=[self.id])
 
+    @property
+    def matrix_cache_key(self):
+        return "result-{}".format(self.id)
 
+    @property
+    def matrix(self):
+        # set instance cache
+        if hasattr(self, "_matrix"):
+            return self._matrix
+
+        # set persistent cache
+        matrix = cache.get(self.matrix_cache_key)
+        if matrix is None:
+            matrix = self.np_matrix.ix[:, 6:].as_matrix()
+            assert matrix.dtype == np.int64
+            mPickle = cPickle.dumps(matrix, protocol=2)
+            logging.info("Setting cache...")
+            cache.set(self.matrix_cache_key, mPickle)
+            self._matrix = matrix
+        else:
+            logging.info("Using cache...")
+            matrix = cPickle.loads(matrix)
+        return matrix
+
+    def get_heatmap_dataset(self, start=0, width=-1, aslist=True):
+        matrix = self.matrix
+        shape = matrix.shape
+        response = {
+            "name": self.name,
+            "shape": shape,
+            "start": start,
+        }
+
+        # slice matrix to subset
+        subset = matrix
+        if width > 0:
+            subset = matrix[start:start+width, :]
+            response['width'] = width
+        else:
+            response['width'] = shape[0]
+
+        # resize image if greater than maximum size
+        if width > self.MAX_HEATMAP_WIDTH or width < 1:
+            response["scaled"] = True
+            subset = imresize(subset, (self.MAX_HEATMAP_WIDTH, shape[1]), interp="bilinear")
+        else:
+            response["scaled"] = False
+
+        if aslist:
+            response["matrix"] = subset.tolist()
+        else:
+            response["matrix"] = subset
+
+        return response
