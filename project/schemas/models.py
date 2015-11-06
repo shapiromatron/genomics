b2e91b6ceea04d45b34916a430fdf89453d785aa
Andy Shapiro <shapiromatron@gmail.com>
2015-11-06 14:25:56 -0500

    update schema per latest requirements

--- project/schemas/models.py
+++ project/schemas/models.py
@@ -4,7 +4,8 @@ from django.db import models
 from django.conf import settings
 
 
-# python manage.py graph_models -g -o schemas.png --pydot schemas
+# python manage.py graph_models -g -o schemas.png --pydot schemas && open schemas.py
+
 
 class Dataset(models.Model):
     owner = models.ForeignKey(
@@ -36,22 +37,47 @@ class Dataset(models.Model):
         abstract = True
 
 
+HG19 = 1
+MM9 = 2
+GENOME_ASSEMBLY_CHOICES = (
+    (HG19, 'hg19'),
+    (MM9,  'mm9'),
+)
+
+
 class GenomicDataset(Dataset):
-    hg19 = 1
-    mm9 = 2
-    GENOME_ASSEMBLY_CHOICES = (
-        (hg19, 'hg19'),
-        (mm9,  'mm9'),
-    )
     genome_assembly = models.PositiveSmallIntegerField(
         choices=GENOME_ASSEMBLY_CHOICES)
     binary = models.FileField(
         max_length=256)
+
+
+class UserDataset(GenomicDataset):
     url = models.URLField(
         max_length=256,
         null=True)
     expiration_date = models.DateTimeField(
         null=True)
+
+
+class EncodeDataset(GenomicDataset):
+    data_type = models.CharField(
+        max_length=16)
+    antibody = models.CharField(
+        max_length=32,
+        blank=True)
+    rna_extract = models.CharField(
+        max_length=32,
+        blank=True)
+    treatment = models.CharField(
+        max_length=32,
+        blank=True)
+    phase = models.CharField(
+        max_length=32,
+        blank=True)
+    localization = models.CharField(
+        max_length=32,
+        blank=True)
 
 
 class FeatureList(Dataset):
@@ -64,6 +90,22 @@ class SortVector(Dataset):
         blank=True)
 
 
+class AnalysisDatasets(models.Model):
+    analysis = models.ForeignKey(
+        'Analysis')
+    dataset = models.ForeignKey(
+        'GenomicDataset')
+    display_name = models.CharField(
+        max_length=128)
+    intermediate_matrix = models.ForeignKey(
+        'IntermediateMatrix',
+        null=True)
+    created = models.DateTimeField(
+        auto_now_add=True)
+    last_updated = models.DateTimeField(
+        auto_now=True)
+
+
 class Analysis(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL)
@@ -72,7 +114,9 @@ class Analysis(models.Model):
     description = models.TextField(
         blank=True)
     datasets = models.ManyToManyField(
-        GenomicDataset)
+        GenomicDataset,
+        through=AnalysisDatasets,
+        through_fields=('analysis', 'dataset'))
     features = models.ForeignKey(
         FeatureList)
     sort_vector = models.ForeignKey(
@@ -93,10 +137,25 @@ class Analysis(models.Model):
         auto_now=True)
 
 
-class Results(models.Model):
+class IntermediateMatrix(models.Model):
+    feature_list = models.ForeignKey(
+        FeatureList,
+        related_name="intermediates")
+    dataset = models.ForeignKey(
+        GenomicDataset,
+        related_name="intermediates")
+    matrix = models.FileField(
+        max_length=256)
+    created = models.DateTimeField(
+        auto_now_add=True)
+    last_updated = models.DateTimeField(
+        auto_now=True)
+
+
+class DatasetCorrelationMatrix(models.Model):
     analysis = models.OneToOneField(Analysis)
-    dont_really_know = models.BooleanField(
-        default=False)
+    matrix = models.FileField(
+        max_length=256)
     created = models.DateTimeField(
         auto_now_add=True)
     last_updated = models.DateTimeField(
