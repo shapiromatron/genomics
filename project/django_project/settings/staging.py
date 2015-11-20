490e3ec783f8f8e0dfa722e71c3ebb00a58eba06
Andy Shapiro <shapiromatron@gmail.com>
2015-11-20 13:59:07 -0500

    update environment variables for production

--- project/django_project/settings/staging.py
+++ project/django_project/settings/staging.py
@@ -12,10 +12,12 @@ DATABASES['default']['USER'] = os.environ.get("DB_USER")
 DATABASES['default']['PASSWORD'] = os.environ.get("DB_PASSWORD")
 
 EMAIL_HOST = os.environ.get("DJANGO_EMAIL_HOST")
-EMAIL_HOST_USER = os.environ.get("DJANGO_EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("DJANGO_EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("DJANGO_EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("DJANGO_EMAIL_PASSWORD")
 EMAIL_PORT = os.environ.get("DJANGO_EMAIL_PORT")
 
 PUBLIC_ROOT = os.environ.get("DJANGO_PUBLIC_PATH")
 STATIC_ROOT = os.path.join(PUBLIC_ROOT, 'static')
 MEDIA_ROOT = os.path.join(PUBLIC_ROOT, 'media')
+
+LOGGING['handlers']['file']['filename'] = os.environ.get("DJANGO_LOG_FULLPATH")
