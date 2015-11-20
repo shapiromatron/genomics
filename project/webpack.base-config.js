e9f989ab00eb512395aa541b6afb21f6633033c7
Andy Shapiro <shapiromatron@gmail.com>
2015-11-20 14:44:58 -0500

    setup webpack for development and production

---
+++ project/webpack.base-config.js
@@ -0,0 +1,57 @@
+var path = require("path"),
+    webpack = require('webpack'),
+    BundleTracker = require('webpack-bundle-tracker'),
+    devFlagPlugin = new webpack.DefinePlugin({__DEBUG__: false});
+
+
+module.exports = {
+
+    __devFlagPlugin: devFlagPlugin,
+
+    context: __dirname,
+
+    entry: [
+        './assets/js/index',
+    ],
+
+    output: {
+        path: path.resolve('./static/bundles/'),
+        filename: '[name]-[hash].js',
+    },
+
+    externals: {},
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new BundleTracker({filename: './webpack-stats.json'}),
+        devFlagPlugin,
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {stage: 0},
+            },
+        ],
+    },
+
+    resolve: {
+        modulesDirectories: [
+            'node_modules',
+            'bower_components',
+        ],
+        extensions: [
+            '',
+            '.js',
+            '.jsx',
+        ],
+    },
+};
--- project/webpack.devserver.js
+++ project/webpack.devserver.js
@@ -1,16 +1,15 @@
-var webpack = require('webpack')
-var WebpackDevServer = require('webpack-dev-server')
-var config = require('./webpack.config')
+var webpack = require('webpack'),
+    WebpackDevServer = require('webpack-dev-server'),
+    config = require('./webpack.dev-config.js');
 
-config.__devFlagPlugin.definitions.__DEBUG__ = true;
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  inline: true,
-  historyApiFallback: true,
-  stats: { colors: true },
+    publicPath: config.output.publicPath,
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    stats: { colors: true },
 }).listen(9001, '0.0.0.0', function (err, result) {
-  if (err) console.log(err);
-  console.log('Listening at 0.0.0.0:9001');
-})
+    if (err) console.log(err);
+    console.log('Listening at 0.0.0.0:9001');
+});
