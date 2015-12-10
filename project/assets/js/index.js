7c18ca62a22b7e35463777b4979fc620eb48672f
Andy Shapiro <shapiromatron@gmail.com>
2015-12-10 15:44:18 -0500

    remove default routing config

--- project/assets/js/index.js
+++ project/assets/js/index.js
@@ -8,9 +8,6 @@ import { Route } from "react-router";
 import { Provider } from 'react-redux';
 import { createHistory } from 'history';
 
-import App from './containers/App';
-import Child from './components/Child';
-import Parent from './components/Parent';
 import reducer from './reducers';
 
 import { devMiddleware, renderDevTools } from './utils/devTools';
@@ -27,12 +24,6 @@ class Root extends Component {
             <div>
                 <Provider store={store}>
                     <ReduxRouter>
-                        <Route path="/dashboard/" component={App}>
-                            <Route path="parent" component={Parent}>
-                                <Route path="child" component={Child} />
-                                <Route path="child/:id" component={Child} />
-                            </Route>
-                        </Route>
                     </ReduxRouter>
                 </Provider>
                 {renderDevTools(store)}
