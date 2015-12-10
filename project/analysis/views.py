176a178c3f34903a52420b653003bfa498b0222d
Andy Shapiro <shapiromatron@gmail.com>
2015-12-10 17:17:35 -0500

    make login-required for dashboard view

--- project/analysis/views.py
+++ project/analysis/views.py
@@ -1,9 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
 
 class Home(TemplateView):
     template_name = "analysis/home.html"
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseRedirect(reverse_lazy('analysis:dashboard'))
+        return super(Home, self).get(request, *args, **kwargs)
+
 
 class Dashboard(TemplateView):
     template_name = "analysis/dashboard.html"
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
