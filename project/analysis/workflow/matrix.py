9e320c6a13c72ac5a09bc499c8cec576885731f7
lavenderca <c.andrew.lavender@gmail.com>
2016-01-05 17:35:48 -0500

    Minor bug fixes, changes to argument parsing, changes to allow compatibility with Python 2

--- project/analysis/workflow/matrix.py
+++ project/analysis/workflow/matrix.py
@@ -3,7 +3,7 @@
 import click
 import os
 from subprocess import call
-
+from six import string_types
 
 class BedMatrix(object):
 
@@ -14,59 +14,50 @@ class BedMatrix(object):
     temp_int_minus_fn = "temp_intersection.minus.tab"
     temp_int_unstranded_fn = "temp_intersection.tab"
 
-    def __init__(self, *args,
-                 anchor='center', bin_start=-2500, bin_number=50,
-                 bin_size=100, opposite_strand_fn=None):
-
-        if len(args) == 3:
-            self.stranded_bigwigs = False
-            self.stranded_bed = False
-        elif len(args) == 4:
-            self.stranded_bigwigs = True
-            self.stranded_bed = True
-        else:
-            raise ValueError("Unknown argument number")
-
-        # Type checks
-        assert anchor in self.ANCHOR_OPTIONS
-        assert isinstance(bin_start, int)
-        assert isinstance(bin_number, int)
-        assert isinstance(bin_size, int)
-        if opposite_strand_fn is not None:
-            assert isinstance(opposite_strand_fn, str)
-            assert os.path.exists(os.path.dirname(opposite_strand_fn))
-            if not self.stranded_bed:
-                raise ValueError("Cannot report opposite strand coverage without stranded_bed flag!!")
-
-        plus_bigwig = None
-        minus_bigwig = None
-        unstranded_bigwig = None
-        feature_bed = None
-        output_matrix = None
-
-        if self.stranded_bigwigs:
-            plus_bigwig, minus_bigwig, feature_bed, output_matrix = args
-            assert os.path.exists(plus_bigwig)
-            assert os.path.exists(minus_bigwig)
-        else:
-            unstranded_bigwig, feature_bed, output_matrix = args
-            assert os.path.exists(unstranded_bigwig)
-
-        assert os.path.exists(feature_bed)
-        assert os.path.exists(os.path.dirname(output_matrix))
-
+    def __init__(self, bigwigs, feature_bed, output_matrix, anchor, bin_start,
+        bin_number, bin_size, opposite_strand_fn, stranded_bigwigs,
+        stranded_bed):
+        
         # Set instance variables
+        self.feature_bed = feature_bed
+        self.output_matrix = output_matrix
         self.anchor = anchor
         self.bin_start = bin_start
         self.bin_number = bin_number
         self.bin_size = bin_size
         self.opposite_strand_fn = opposite_strand_fn
-        self.plus_bigwig = plus_bigwig
-        self.minus_bigwig = minus_bigwig
-        self.unstranded_bigwig = unstranded_bigwig
-        self.feature_bed = feature_bed
-        self.output_matrix = output_matrix
-
+        self.stranded_bigwigs = stranded_bigwigs
+        if self.stranded_bigwigs:
+            if len(bigwigs) != 2:
+                raise ValueError("Two paired bigwig files are expected!!")
+            self.plus_bigwig = bigwigs[0]
+            self.minus_bigwig = bigwigs[1]
+        else:
+            if len(bigwigs) != 1:
+                raise ValueError("One unstranded bigwig file is expected!!")
+            self.unstranded_bigwig = bigwigs[0]
+        self.stranded_bed = stranded_bed
+        
+        # Type checks
+        assert anchor in self.ANCHOR_OPTIONS
+        assert isinstance(self.bin_start, int)
+        assert isinstance(self.bin_number, int)
+        assert isinstance(self.bin_size, int)
+        if self.opposite_strand_fn is not None:
+            assert isinstance(self.opposite_strand_fn, string_types)
+            if os.path.dirname(self.output_matrix) != "":
+                assert os.path.exists(os.path.dirname(self.opposite_strand_fn))
+            if not self.stranded_bed:
+                raise ValueError("Cannot report opposite strand coverage without stranded_bed flag!!")
+        if self.stranded_bigwigs:
+            assert os.path.exists(self.plus_bigwig)
+            assert os.path.exists(self.minus_bigwig)
+        else:
+            assert os.path.exists(self.unstranded_bigwig)
+        assert os.path.exists(self.feature_bed)
+        if os.path.dirname(self.output_matrix) != "":
+            assert os.path.exists(os.path.dirname(self.output_matrix))
+        
         # Execute
         self.make_bed()
         self.bigwig_average_over_bed()
@@ -225,9 +216,9 @@ class BedMatrix(object):
     def make_matrix(self):
         # Create output matrix files
         if self.stranded_bigwigs:
-            self.make_unstranded_matrix()
-        else:
             self.make_stranded_matrix()
+        else:
+            self.make_unstranded_matrix()
 
     def make_unstranded_matrix(self):
         # Create output matrix for unstranded bigwig
@@ -349,31 +340,8 @@ class BedMatrix(object):
                 OPPOSITE.write("\n")
 
 
-@click.group()
-def cli():
-    """
-    Generate matrices for stranded or unstranded bigWig matrices
-    """
-
-
-@cli.command('unstranded')
-@click.argument('unstranded_bigwig')
-@click.argument('feature_bed')
-@click.argument('output_matrix')
-@click.option('-a', '--anchor', default='center', help='Bin anchor', type=click.Choice(BedMatrix.ANCHOR_OPTIONS))
-@click.option('-b', '--bin_start', default=-2500, help='Relative bin start', type=int)
-@click.option('-n', '--bin_number', default=50, help='Number of bins', type=int)
-@click.option('-s', '--bin_size', default=100, help='Size of bins', type=int)
-def unstranded(unstranded_bigwig, feature_bed, output_matrix, **kwargs):
-    """
-    Run with unstranded matrix
-    """
-    BedMatrix(unstranded_bigwig, feature_bed, output_matrix, **kwargs)
-
-
-@cli.command('stranded')
-@click.argument('plus_bigwig')
-@click.argument('minus_bigwig')
+@click.command()
+@click.argument('bigwigs', nargs=-1)
 @click.argument('feature_bed')
 @click.argument('output_matrix')
 @click.option('-a', '--anchor', default='center', help='Bin anchor', type=click.Choice(BedMatrix.ANCHOR_OPTIONS))
@@ -381,12 +349,17 @@ def unstranded(unstranded_bigwig, feature_bed, output_matrix, **kwargs):
 @click.option('-n', '--bin_number', default=50, help='Number of bins', type=int)
 @click.option('-s', '--bin_size', default=100, help='Size of bins', type=int)
 @click.option('--opposite_strand_fn', type=str, help='Output filename for opposite strand coverage')
-def stranded(plus_bigwig, minus_bigwig, feature_bed, output_matrix, **kwargs):
-    """
-    Run with stranded matrix
-    """
-    BedMatrix(plus_bigwig, minus_bigwig, feature_bed, output_matrix, **kwargs)
+@click.option('--stranded_bigwigs', is_flag=True, help='Expect stranded, paired bigwigs with plus/forward strand first')
+@click.option('--stranded_bed', is_flag=True, help='Expect stranded bed')
 
+def cli(bigwigs, feature_bed, output_matrix, anchor, bin_start, bin_number,
+    bin_size, opposite_strand_fn, stranded_bigwigs, stranded_bed):
+    """
+    Generate matrices for stranded or unstranded bigWig matrices
+    """
+    BedMatrix(bigwigs, feature_bed, output_matrix, anchor, bin_start,
+        bin_number, bin_size, opposite_strand_fn, stranded_bigwigs,
+        stranded_bed)
 
 if __name__ == '__main__':
     cli()
