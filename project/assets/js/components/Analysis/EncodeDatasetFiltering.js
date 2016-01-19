c8d14f5add91e2903d9fb890f52c5777c63fd0a1
Andy Shapiro <shapiromatron@gmail.com>
2016-01-19 10:29:40 -0500

    revise encode dataset filters

--- project/assets/js/components/Analysis/EncodeDatasetFiltering.js
+++ project/assets/js/components/Analysis/EncodeDatasetFiltering.js
@@ -5,61 +5,59 @@ class EncodeDatasetFiltering extends React.Component {
 
     render () {
         let opts = this.props.options;
-
         return (
             <div className='row'>
 
-                <div className='col-sm-4'>
+                <div className='col-md-4'>
                     <label className='control-label'>Data type</label>
                     <select size='10' className='form-control' multiple={true}>
                         {opts.data_type.map(
-                           (d) => <option key={d} value={d}>{d}</option>
+                           (d, i) => <option key={i} value={d}>{d || '-'}</option>
                         )}
                     </select>
                 </div>
 
-                <div className='col-sm-4'>
+                <div className='col-md-4'>
                     <label className='control-label'>Cell type</label>
                     <select size='10' className='form-control' multiple={true}>
-                        {opts.cell_type.map(
-                           (d) => <option key={d} value={d}>{d}</option>
+                        {opts.data_type.map(
+                           (d, i) => <option key={i} value={d}>{d || '-'}</option>
                         )}
                     </select>
                 </div>
 
-                <div className='col-sm-4'>
+                <div className='col-md-4'>
                     <label className='control-label'>Treatment</label>
                     <select size='10' className='form-control' multiple={true}>
                         {opts.treatment.map(
-                           (d) => <option key={d} value={d}>{d}</option>
+                           (d, i) => <option key={i} value={d}>{d || '-'}</option>
                         )}
                     </select>
                 </div>
 
-                <div className='col-sm-4'>
-                    <label className='control-label'>Phase</label>
-                    <select size='10' className='form-control' multiple={true}>
-                        {opts.phase.map(
-                           (d) => <option key={d} value={d}>{d}</option>
-                        )}
-                    </select>
-                </div>
-
-                <div className='col-sm-4'>
-                    <label className='control-label'>RNA extract</label>
-                    <select size='10' className='form-control' multiple={true}>
-                        {opts.rna_extract.map(
-                           (d) => <option key={d} value={d}>{d}</option>
-                        )}
-                    </select>
-                </div>
-
-
-                <div className='col-sm-4'>
+                <div className='col-md-4'>
                     <label className='control-label'>Antibody</label>
                     <select size='10' className='form-control' multiple={true}>
                         {opts.antibody.map(
-                           (d) => <option key={d} value={d}>{d}</option>
+                           (d, i) => <option key={i} value={d}>{d}</option>
+                        )}
+                    </select>
+                </div>
+
+                <div className='col-md-4'>
+                    <label className='control-label'>Phase</label>
+                    <select size='10' className='form-control' multiple={true}>
+                        {opts.phase.map(
+                           (d, i) => <option key={i} value={d}>{d || '-'}</option>
+                        )}
+                    </select>
+                </div>
+
+                <div className='col-md-4'>
+                    <label className='control-label'>RNA extract</label>
+                    <select size='10' className='form-control' multiple={true}>
+                        {opts.rna_extract.map(
+                           (d, i) => <option key={i} value={d}>{d || '-'}</option>
                         )}
                     </select>
                 </div>
