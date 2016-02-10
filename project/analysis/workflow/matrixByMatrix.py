f0a56993b8d85f93c91dcae44df4b332cbaf11fa
lavenderca <c.andrew.lavender@gmail.com>
2016-02-10 11:08:53 -0500

    Cleaned up redundant functions

--- project/analysis/workflow/matrixByMatrix.py
+++ project/analysis/workflow/matrixByMatrix.py
@@ -58,7 +58,7 @@ class MatrixByMatrix():
             self.sort_orders.append({"data_set": self.matrix_names[i], "sort_order":sorted(matrix_entries, key=lambda x: (matrix_entries[x]["row_sum"]), reverse=True)})
 
     def createVectorList(self):
-        self.vector_list = []
+        vector_list = []
         
         for fn in self.matrix_files:
             
@@ -68,35 +68,53 @@ class MatrixByMatrix():
             )
             
             vector = numpy.sum(matrix, axis=0)
-            self.vector_list.append(vector)
+            vector_list.append(vector)
+        
+        return vector_list
 
     def createCorrelationMatrix(self):
         self.correlation_matrix = []
         
-        for i in range(len(self.vector_list)):
-            self.correlation_matrix.append([])
-            for j in range(len(self.vector_list)):
-                if i == j:
-                    self.correlation_matrix[-1].append(1)
-                else:
-                    try:
-                        self.correlation_matrix[j][i]
-                    except IndexError:
-                        self.correlation_matrix[-1].append(stats.spearmanr(self.vector_list[i], self.vector_list[j])[0])
+        if self.sort_vector:
+            sort_vector = self.readInSortVector(self.sort_vector)
+            for matrix in self.matrix_files:
+                self.correlation_matrix.append(
+                    self.findVectorMatrixCorr(
+                        sort_vector, matrix, self.bin_number
+                    )
+                )
+        else:
+            vector_list = self.createVectorList()
+            for i in range(len(vector_list)):
+                self.correlation_matrix.append([])
+                for j in range(len(vector_list)):
+                    if i == j:
+                        self.correlation_matrix[-1].append(1)
                     else:
-                        self.correlation_matrix[-1].append(self.correlation_matrix[j][i])
+                        try:
+                            self.correlation_matrix[j][i]
+                        except IndexError:
+                            self.correlation_matrix[-1].append(stats.spearmanr(vector_list[i], vector_list[j])[0])
+                        else:
+                            self.correlation_matrix[-1].append(self.correlation_matrix[j][i])
     
     def createDistanceMatrix(self):
-        self.distance_matrix = []
+        distance_matrix = []
         
         for i in range(len(self.correlation_matrix)):
-            self.distance_matrix.append([])
+            distance_matrix.append([])
             for j in range(len(self.correlation_matrix[i])):
-                self.distance_matrix[-1].append(1 - self.correlation_matrix[i][j])
+                distance_matrix[-1].append(1 - self.correlation_matrix[i][j])
+        
+        return distance_matrix
 
     def performClustering(self):
-        distance_array = numpy.array(self.distance_matrix)
-        lnk = linkage(squareform(distance_array), method="average")
+        if self.sort_vector:
+            lnk = linkage(numpy.array(self.correlation_matrix), method="average")
+        else:
+            distance_matrix = self.createDistanceMatrix()
+            distance_array = numpy.array(distance_matrix)
+            lnk = linkage(squareform(distance_array), method="average")
         
         full_dg = dendrogram(lnk)
         truncated_dg = dendrogram(lnk,p=50,truncate_mode="lastp")
@@ -139,13 +157,21 @@ class MatrixByMatrix():
         
         for i in range(len(self.cluster_members)):
             self.cluster_correlation_values.append([])
-            for j in range(len(self.cluster_members)):
-                self.cluster_correlation_values[-1].append([])
-                for k in range(len(self.cluster_members[i])):
-                    for l in range(len(self.cluster_members[j])):
-                        index_1 = self.matrix_names.index(self.cluster_members[i][k])
-                        index_2 = self.matrix_names.index(self.cluster_members[j][l])
-                        self.cluster_correlation_values[-1][-1].append(self.correlation_matrix[index_1][index_2])
+            if self.sort_vector:
+                for j in range(len(self.cluster_members[i])):
+                    index = self.matrix_names.index(self.cluster_members[i][j])
+                    for k in range(len(self.correlation_matrix[index])):
+                        if j == 0:    
+                            self.cluster_correlation_values[-1].append([])
+                        self.cluster_correlation_values[-1][k].append(self.correlation_matrix[index][k])
+            else:
+                for j in range(len(self.cluster_members)):
+                    self.cluster_correlation_values[-1].append([])
+                    for k in range(len(self.cluster_members[i])):
+                        for l in range(len(self.cluster_members[j])):
+                            index_1 = self.matrix_names.index(self.cluster_members[i][k])
+                            index_2 = self.matrix_names.index(self.cluster_members[j][l])
+                            self.cluster_correlation_values[-1][-1].append(self.correlation_matrix[index_1][index_2])
         
         self.max_correlation_values = []
         self.med_correlation_values = []
@@ -203,77 +229,13 @@ class MatrixByMatrix():
             lst.append(stats.spearmanr(vector, input_matrix[i])[0])
         
         return lst
-
-    def createSVCorrelationMatrix(self):
-        sort_vector = self.readInSortVector(self.sort_vector)
-        self.correlation_matrix = []
-        
-        for matrix in self.matrix_files:
-            self.correlation_matrix.append(
-                self.findVectorMatrixCorr(
-                    sort_vector, matrix, self.bin_number
-                )
-            )
-
-    def performSVClustering(self):
-        lnk = linkage(numpy.array(self.correlation_matrix), method="average")
-        
-        full_dg = dendrogram(lnk)
-        truncated_dg = dendrogram(lnk,p=50,truncate_mode="lastp")
-        
-        self.dendrogram = truncated_dg
-        self.cluster_members = []
-        
-        full_id = 0
-        for entry in truncated_dg['ivl']:
-            self.cluster_members.append([])
-            if len(entry.split("(")) > 1:
-                member_num = int(entry.split("(")[1].split(")")[0])
-                for i in range(member_num):
-                    self.cluster_members[-1].append(self.matrix_names[int(full_dg['ivl'][full_id])])
-                    full_id += 1
-            else:
-                self.cluster_members[-1].append(self.matrix_names[int(full_dg['ivl'][full_id])])
-                full_id += 1
-
-    def findSVClusterCorrelationValues(self):
-        self.cluster_correlation_values = []
-        
-        for i in range(len(self.cluster_members)):
-            self.cluster_correlation_values.append([])
-            for j in range(len(self.cluster_members[i])):
-                index = self.matrix_names.index(self.cluster_members[i][j])
-                for k in range(len(self.correlation_matrix[index])):
-                    if j == 0:    
-                        self.cluster_correlation_values[-1].append([])
-                    self.cluster_correlation_values[-1][k].append(self.correlation_matrix[index][k])
-        
-        self.max_correlation_values = []
-        self.med_correlation_values = []
-        self.max_abs_correlation_values = []
-        
-        for i in range(len(self.cluster_correlation_values)):
-            self.max_correlation_values.append([])
-            self.med_correlation_values.append([])
-            self.max_abs_correlation_values.append([])
-            for j in range(len(self.cluster_correlation_values)):
-                self.max_correlation_values[-1].append(max(self.cluster_correlation_values[i][j]))
-                self.med_correlation_values[-1].append(numpy.median(self.cluster_correlation_values[i][j]))
-                self.max_abs_correlation_values[-1].append(self.maxAbs(self.cluster_correlation_values[i][j]))
     
     def execute(self):
         self.readMatrixFiles()
         self.createSortOrders()
-        if self.sort_vector:
-            self.createSVCorrelationMatrix()
-            self.performSVClustering()
-            self.findSVClusterCorrelationValues()
-        else:
-            self.createVectorList()
-            self.createCorrelationMatrix()
-            self.createDistanceMatrix()
-            self.performClustering()
-            self.findClusterCorrelationValues()
+        self.createCorrelationMatrix()
+        self.performClustering()
+        self.findClusterCorrelationValues()
         self.writeJson()
 
 @click.command()
