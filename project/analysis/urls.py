7a85b66f35b1e81d74c03e2f3221ef210d4c21eb
Andy Shapiro <shapiromatron@gmail.com>
2016-02-11 17:26:29 -0500

    create temporary view for visualization

--- project/analysis/urls.py
+++ project/analysis/urls.py
@@ -9,12 +9,17 @@ router.register('user-dataset', api.UserDatasetViewset, base_name='user-dataset'
 router.register('encode-dataset', api.EncodeDatasetViewset, base_name='encode-dataset')
 router.register('feature-list', api.FeatureListViewset, base_name='feature-list')
 router.register('sort-vector', api.SortVectorViewset, base_name='sort-vector')
+router.register('feature-list-count-matrix', api.FeatureListCountMatrixViewset, base_name="flcm")
 
 
 urlpatterns = [
 
     url(r'^api/',
         include(router.urls, namespace='api')),
+
+    url(r'^analysis/(?P<pk>\d+)/visuals/$',
+        views.VisualTestingObject.as_view(),
+        name='visual_testing'),
 
     url(r'^analysis/(?P<pk>\d+)/execute/$',
         views.Execute.as_view(),
