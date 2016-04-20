4d8b6f435adc27868827e19f5a9d5e1d55e6bfac
Andy Lavender <c.andrew.lavender@gmail.com>
2016-04-20 16:47:48 -0400

    Added js classes for feature clustering visualization

---
+++ project/assets/js/AnalysisVisual/FeatureClusteringOverview.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import d3 from 'd3';
+
+
+class FeatureClusteringOverview{
+
+    constructor(el, data) {
+
+    }
+
+    render() {
+
+    }
+}
+
+
+export default FeatureClusteringOverview;
