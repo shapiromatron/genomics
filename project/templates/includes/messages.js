a9950c5e2c57a79af15eeca38e696cd26658dcd3
Andy Shapiro <shapiromatron@gmail.com>
2016-05-19 13:09:06 -0400

    make sure toastr options work with all notifications

--- project/templates/includes/messages.js
+++ project/templates/includes/messages.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-    toastr.options = {
+    var opts = {
         closeButton: true,
         debug: false,
         newestOnTop: false,
@@ -18,15 +18,14 @@
     };
 
     {% for message in messages %}
-        toastr["{{message.tags}}"]("{{ message|safe }}")
+        toastr["{{message.tags}}"]("{{ message|safe }}", opts)
     {% endfor %}
 
     window.setInterval(function(){
         $.get('{% url "analysis:poll_messages" %}', function(d){
             d.messages.forEach(function(resp){
-                toastr[resp.status](resp.message);
+                toastr[resp.status](resp.message, opts);
             });
         });
-    }, 10000);
-
+    }, 5000);
 </script>
